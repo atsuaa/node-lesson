a8f0e75dd1f0ecb695a9c7e019ecab29fad92307
Atsushi Mori <sushidan.am618@gmail.com>
2020-12-31 00:24:58 +0900

    lesson2

---
+++ messages.js
@@ -0,0 +1,7 @@
+"use strict";
+// メッセージを配列でリストにする
+let messages = [
+  "A change of environment can be a good thing!!",
+  "You will make it",
+  "Just run with the code!"
+];
---
+++ printer.js
@@ -0,0 +1,3 @@
+"use strict";
+let x = "Universe";
+console.log(`Hello, ${x}`); // 補完した文字列をログに出す
